d17d4214ff3a1e4f0dcd3385124882a2b90a5268
Robi <robelmezemir@gmail.com>
2025-01-02 18:17:35 +0300

    connect to postgres database and run better-auth migrations

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 npx sv create
 
 pnpm add better-auth
+
+create .env file
+
+create src/auth.ts file
+
+install dotenv  
+
+decide to use postgres
+
+create src/db.ts file
+
+install pg and types
+pnpm i --save-dev @types/node
+pnpm i --save-dev @types/pg
+
+added a postgres url to .env
+
+npx @better-auth/cli generate
--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,20 @@
 import { betterAuth } from "better-auth";
- 
+import dotenv from "dotenv";
+
+import { db } from "./db";
+
+dotenv.config();
+
 export const auth = betterAuth({
-    //...
-})
+  database: db,
+
+  emailAndPassword: {
+    enabled: true
+  },
+  // socialProviders: {
+  //   github: {
+  //     clientId: process.env.GITHUB_CLIENT_ID as string,
+  //     clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+  //   }
+  // },
+});
